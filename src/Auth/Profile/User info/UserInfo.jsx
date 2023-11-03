416e721a46d4e3c9736a5e4b20e8e2456e8048bb
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-11-03 17:44:21 +0200

    profile design

--- src/Auth/Profile/User info/UserInfo.jsx
+++ src/Auth/Profile/User info/UserInfo.jsx
@@ -9,37 +9,37 @@ function UserInfo(props) {
   return (
     <React.Fragment>
       <div className="UserInfo">
-        <div className="left">
-          <img
-            src={`${process.env.REACT_APP_API_UPLOADS}/${props.ProfileData.Avatar}`}
-            alt={`${props.ProfileData.FirstName} ${props.ProfileData.LastName}`}
-          />
-          <div className="data">
-            <h3>
-              {props.ProfileData.FirstName} {props.ProfileData.LastName}
-            </h3>
-            <p>{props.ProfileData.bio}</p>
-            <div className="social">
-              {props.ProfileData.Links
-                ? props.ProfileData?.Links.map((Link) => (
-                    <a href={Link.Link}>
-                      <i class={Link.Icon} />
-                      <span>{Link.Name}</span>
-                    </a>
-                  ))
-                : null}
-            </div>
-            {_id === param.User_id ? null : (
-              <div className="follow-action">
-                <button>Follow</button>
-              </div>
-            )}
+        <img
+          src={`${process.env.REACT_APP_API_UPLOADS}/${props.ProfileData.Avatar}`}
+          alt={`${props.ProfileData.FirstName} ${props.ProfileData.LastName}`}
+        />
+        <div className="data">
+          <h3>
+            {props.ProfileData.FirstName} {props.ProfileData.LastName}
+          </h3>
+          <p>{props.ProfileData.bio}</p>
+          <div className="social">
+            {props.ProfileData.Links
+              ? props.ProfileData?.Links.map((Link) => (
+                  <a href={Link.Link}>
+                    <i class={Link.Icon} />
+                    <span>{Link.Name}</span>
+                  </a>
+                ))
+              : null}
           </div>
-        </div>
-        <div className="right">
-          <Link to={`/User/${param.User_id}/Setting`}>
-            <i className="fa-solid fa-gears"></i>
-          </Link>
+          {_id === param.User_id ? (
+            <div className="button-action">
+              <Link to={`/User/${param.User_id}/Setting`}>
+                <i className="fa-solid fa-gears" />
+                Edit your profile
+              </Link>
+            </div>
+          ) : (
+            <div className="button-action">
+              <button>Follow</button>
+            </div>
+          )}
         </div>
       </div>
     </React.Fragment>
